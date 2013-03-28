514d7d2262fbc607860a964569e723144981bbe6
Ilja Iwas <ilja@iwascoding.com>
2013-03-28 17:57:17 +0100

    Initial Commit

---
+++ cocoahub/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  cocoahub
+//
+//  Created by ilja on 28.03.13.
+//  Copyright (c) 2013 iwascoding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
