bc8633c755e29f52246b4457ac6524656b3c2301
Ilja Iwas <ilja@iwascoding.com>
2013-04-02 16:54:27 +0200

    changes to build+run script

--- build+run.sh
+++ build+run.sh
@@ -4,6 +4,7 @@ killall cocoahub
 if [ -d ./bin ]; then
 	mkdir bin
 fi
+pod install
 pod update
 xcodebuild -workspace cocoahub.xcworkspace/ -scheme cocoahub install "DSTROOT=./bin" "INSTALL_PATH=/"
 bin/cocoahub & 
