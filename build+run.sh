03d19769baf73cae648745e78a4146fe1492d177
Ilja Iwas <ilja@iwascoding.com>
2013-04-02 17:44:27 +0200

    fixed build+run script

--- build+run.sh
+++ build+run.sh
@@ -7,4 +7,4 @@ fi
 pod install
 pod update
 xcodebuild -workspace cocoahub.xcworkspace/ -scheme cocoahub install "DSTROOT=./bin" "INSTALL_PATH=/"
-bin/cocoahub 2>&1 >/dev/null  &
+bin/cocoahub 2&>1 >/dev/null  &
